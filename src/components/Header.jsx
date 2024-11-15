c8791ef0de5d9fe1f8ee636483d5e9d53458ae9e
Julian Camarero <juliancamarero_trico@hotmail.com>
2024-11-15 12:27:49 -0300

    Header modificado y footer basico

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Header.css';
+import homeImage from '../assets/pokemon.png';
 
 function Header() {
   return (
-    <header className="bg-primary text-white py-3 text-center">
-      <h1 className='display-4'>Pokémon App</h1>
+    <header className="header-custom">
+      <div className="image-container">
+        <img src={homeImage} alt="Pokémon" />
+      </div>
     </header>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,12 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+document.getElementById('search-btn').addEventListener('click', () => {
+  const spinner = document.getElementById('loading-spinner');
+  spinner.style.display = 'inline-block'; // Muestra el spinner al iniciar la búsqueda
+
+  // Simula una búsqueda (reemplaza esto con tu lógica real)
+  setTimeout(() => {
+    spinner.style.display = 'none'; // Oculta el spinner después de 2 segundos
+  }, 2000);
+});
